abbecc84b87e89c9a187e45fe7fb15583c38026e
EC2 Default User <ec2-user@ip-172-31-9-117.ap-south-1.compute.internal>
2026-07-01 17:19:15 +0000

    hello

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    logging.info("Home endpoint called!!!great")
+    logging.info("Home endpoint called!!!hello")
     return "Hello from Flask app"
 
 if __name__ == "__main__":
